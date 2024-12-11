6a74230f8b46aad557551760d4d822a79d4d33ff
varun bachalli <varun_bachalli@hotmail.com>
2024-12-11 11:50:09 +0530

    testing rdkit

--- src/app/api/pdf/test/route.ts
+++ src/app/api/pdf/test/route.ts
@@ -1,19 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import path from "path";
 import fs from "fs";
+import initRDKitModule from "./RDKIT_minimal";
 export async function GET(request: NextRequest) {
     const existsTest = [
         path.join(process.cwd(), "rdkit/RDKit_minimal.wasm"),
         path.join(process.cwd(), "app/api/pdf/test/rdkit/RDKit_minimal.wasm")
     ];
 
-    // const readDir = fs.readdirSync(path.join(process.cwd(), "rdkit"));
-
-    const existsResponse = existsTest.map((exists) => {
-        return {
-            [exists]: fs.existsSync(exists)
-        };
+    const rdkit = await initRDKitModule({
+        locateFile: () => path.join(process.cwd(), "rdkit/RDKit_minimal.wasm")
     });
 
-    return NextResponse.json({ message: existsResponse, request });
+    var smiles = "CC(=O)Oc1ccccc1C(=O)O";
+    var mol = rdkit.get_mol(smiles);
+    var svg = mol.get_svg();
+
+    // const readDir = fs.readdirSync(path.join(process.cwd(), "rdkit"));
+
+    // const existsResponse = existsTest.map((exists) => {
+    //     return {
+    //         [exists]: fs.existsSync(exists)
+    //     };
+    // });
+
+    return NextResponse.json({ message: svg, request });
 }
