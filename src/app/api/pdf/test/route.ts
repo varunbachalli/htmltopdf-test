ec05368a01342107524d84ab0a8bdde0edf5eeb0
varun bachalli <varun_bachalli@hotmail.com>
2024-12-11 11:33:09 +0530

    moved docs

--- src/app/api/pdf/test/route.ts
+++ src/app/api/pdf/test/route.ts
@@ -3,9 +3,8 @@ import path from "path";
 import fs from "fs";
 export async function GET(request: NextRequest) {
     const existsTest = [
-        path.join(process.cwd(), "src/app/rdkit/RDKit_minimal.wasm"),
         path.join(process.cwd(), "rdkit/RDKit_minimal.wasm"),
-        path.join(process.cwd(), "app/rdkit/RDKit_minimal.wasm")
+        path.join(process.cwd(), "app/api/pdf/test/rdkit/RDKit_minimal.wasm")
     ];
 
     const existsResponse = existsTest.map((exists) => {
